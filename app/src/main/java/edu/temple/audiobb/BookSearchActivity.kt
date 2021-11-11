fa9745a36c9393aace0f155defd2182321a3f298
playerplayer <bohdan.k2000@gmail.com>
2021-11-11 06:29:39 -0500

    Implemented Get Request and Theme/Style

--- app/src/main/java/edu/temple/audiobb/BookSearchActivity.kt
+++ app/src/main/java/edu/temple/audiobb/BookSearchActivity.kt
@@ -1,11 +1,82 @@
 package edu.temple.audiobb
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 class BookSearchActivity : AppCompatActivity() {
+    lateinit var editField: EditText
+    lateinit var btnCancel: Button
+    lateinit var btnSearch: Button
+    val requestQueue = Volley.newRequestQueue(this)
+    val url = "https://kamorris.com/lab/cis3515/search.php?term="
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_search)
+
+
+        editField = findViewById(R.id.editField)
+        btnCancel = findViewById(R.id.btnCancel)
+        btnSearch = findViewById(R.id.btnSearch)
+
+        btnCancel.setOnClickListener {
+            val i = Intent()
+            setResult(RESULT_OK, i)
+            this.finish()
+        }
+        btnSearch.setOnClickListener(View.OnClickListener {
+
+            val jsonArrayRequest = JsonArrayRequest(
+                Request.Method.GET, url + editField.text.toString(), null,
+                { response ->
+
+                    val intent = Intent()
+                    val returnBook: BookList = jsonToBookList(response)
+                    intent.putExtra("result", response.toString())
+                    intent.putExtra("SearchBooks", returnBook)
+
+                    setResult(RESULT_OK, intent)
+                    finish()
+
+                }
+            ) { error ->
+            }
+            requestQueue.add(jsonArrayRequest)
+        })
+    }
+
+    /*
+        val title: String,
+        val author: String,
+        val id: Int,
+        val coverURL: String
+     */
+    private fun jsonToBookList(jsonArray: JSONArray): BookList {
+        val bookList = BookList()
+        var book: JSONObject
+        for (i in 0 until jsonArray.length()) {
+            try {
+                book = jsonArray.getJSONObject(i)
+                val temp = Book(
+                    book.getString("title"),
+                    book.getString("author"),
+                    book.getInt("id"),
+                    book.getString("cover_url")
+                )
+                bookList.add(temp)
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        }
+        return bookList
+
     }
 }
